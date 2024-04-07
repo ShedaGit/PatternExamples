05615bd46b89ed0a6e536fc9d967398dac639cb8
Daniel Shelestov <fellowers.flobot@gmail.com>
2024-04-07 17:38:21 +0300

    add simple usage examle

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Strategy.Infrastructure;
+using Strategy.Infrastructure.Strategies;
+
+var cityDuckDisplayStrategy = new SimpleDisplayStrategy("City Duck");
+var simpleQuackStrategy = new SimpleQuackStrategy();
+var simpleFlyStrategy = new SimpleFlyStrategy();
+
+var rubberDuckDisplayStrategy = new SimpleDisplayStrategy("Rubber Duck");
+var noQuackStrategy = new NoQuackStrategy();
+var noFlyStrategy = new NoFlyStrategy();
+
+var cityDuck = new Duck(cityDuckDisplayStrategy, simpleQuackStrategy, simpleFlyStrategy);
+var rubberDuck = new Duck(rubberDuckDisplayStrategy, noQuackStrategy, noFlyStrategy);
+
+DisplayDuck(cityDuck);
+Console.WriteLine(Environment.NewLine);
+DisplayDuck(rubberDuck);
+
+void DisplayDuck(Duck duck) 
+{
+    duck.Display();
+    duck.Quack();
+    duck.Fly();
+}
