2e02e286be7a9d01d5f0e4b72bb903e7630e1261
Daniel Shelestov <fellowers.flobot@gmail.com>
2024-04-07 19:19:26 +0300

    add usage exaple

--- Observer/Program.cs
+++ Observer/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Observer.Infrastructure;
+
+var weatherStationManual = new WeatherStation();
+var phoneA = new PhoneDisplay("A", weatherStationManual);
+var phoneB = new PhoneDisplay("B", weatherStationManual);
+
+weatherStationManual.Add(phoneA);
+weatherStationManual.Add(phoneB);
+
+weatherStationManual.SetTemperature(23);
+weatherStationManual.Notify();
+
+weatherStationManual.Remove(phoneA);
+
+weatherStationManual.SetTemperature(25);
+weatherStationManual.Notify();
+
