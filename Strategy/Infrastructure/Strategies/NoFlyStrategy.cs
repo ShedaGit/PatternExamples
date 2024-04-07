806a44506344f8bb2981b00e082bda96ad6f22e7
Daniel Shelestov <fellowers.flobot@gmail.com>
2024-04-07 17:26:42 +0300

    add inaction strategies

---
+++ Strategy/Infrastructure/Strategies/NoFlyStrategy.cs
@@ -0,0 +1,9 @@
+﻿using Strategy.Infrastructure.Interfaces;
+
+namespace Strategy.Infrastructure.Strategies
+{
+    internal class NoFlyStrategy : IFlyStrategy
+    {
+        public void Fly() { }
+    }
+}
---
+++ Strategy/Infrastructure/Strategies/NoQuackStrategy.cs
@@ -0,0 +1,9 @@
+﻿using Strategy.Infrastructure.Interfaces;
+
+namespace Strategy.Infrastructure.Strategies
+{
+    internal class NoQuackStrategy : IQuackStrategy
+    {
+        public void Quack() { }
+    }
+}
