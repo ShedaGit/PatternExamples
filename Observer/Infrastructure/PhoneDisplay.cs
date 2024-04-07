c3d162f558d8577e6cf5200262ca754c991e3819
Daniel Shelestov <fellowers.flobot@gmail.com>
2024-04-07 19:18:58 +0300

    add simple implementaion

---
+++ Observer/Infrastructure/PhoneDisplay.cs
@@ -0,0 +1,26 @@
+﻿using Observer.Infrastructure.Interfaces;
+
+namespace Observer.Infrastructure
+{
+    public class PhoneDisplay : IObserver
+    {
+        readonly string _name;
+        readonly WeatherStation _station;
+
+        public PhoneDisplay(string phoneName, WeatherStation station)
+        {
+            _name = phoneName;
+            _station = station;
+        }
+
+        public void Update()
+        {
+            DisplayCurrentTemperature(_station.GetTemperature());
+        }
+
+        void DisplayCurrentTemperature(int temperature)
+        {
+            Console.WriteLine($"Phone {_name}: current temperature is {temperature}°C.");
+        }
+    }
+}
---
+++ Observer/Infrastructure/WeatherStation.cs
@@ -0,0 +1,36 @@
+﻿using Observer.Infrastructure.Interfaces;
+
+namespace Observer.Infrastructure
+{
+    public class WeatherStation : IObservable
+    {
+        readonly List<IObserver> _observers;
+
+        int _temperature;
+
+        public WeatherStation()
+        {
+            _observers = [];
+        }
+
+        public void Add(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Remove(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (var observer in _observers)
+                observer.Update();
+        }
+
+        public int GetTemperature() => _temperature;
+
+        public void SetTemperature(int value) => _temperature = value;
+    }
+}
