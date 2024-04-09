0e502090403df4f1e0269b9c8f869ae99ec5c351
Daniel Shelestov <fellowers.flobot@gmail.com>
2024-04-10 00:30:31 +0300

    add simple usage examples

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using AbstractFactory.Infrastructure;
+
+var windowsFactory = new AlertBoxFactory(new WindowsAlertBox(), new WindowsButton());
+var linuxFactory = new AlertBoxFactory(new LinuxAlertBox(), new LinuxButton());
+
+var windowsAlert = windowsFactory.GetProductA();
+var windowsButton = windowsFactory.GetProductB();
+
+Console.WriteLine(windowsAlert.Name + " - " + windowsButton.Name);
+
+var linuxAlert = linuxFactory.GetProductA();
+var linuxButton = linuxFactory.GetProductB();
+
+Console.WriteLine(linuxAlert.Name + " - " + linuxButton.Name);
+
