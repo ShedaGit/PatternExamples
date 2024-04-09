20eb09b21009f22f1442882a180de5adb6d70db6
Daniel Shelestov <fellowers.flobot@gmail.com>
2024-04-10 00:30:20 +0300

    add untyped alert box factory

---
+++ AbstractFactory/Infrastructure/AlertBoxFactory.cs
@@ -0,0 +1,27 @@
+﻿using AbstractFactory.Infrastructure.Interfaces;
+
+namespace AbstractFactory.Infrastructure
+{
+    public class AlertBoxFactory : IFactory
+    {
+        //It can be the same concretion, but constructed differently
+        readonly IProductA _alert;
+        readonly IProductB _button;
+
+        public AlertBoxFactory(IProductA alert, IProductB button)
+        {
+            _alert = alert;
+            _button = button;
+        }
+
+        public IProductA GetProductA()
+        {
+            return _alert;
+        }
+
+        public IProductB GetProductB()
+        {
+            return _button;
+        }
+    }
+}
