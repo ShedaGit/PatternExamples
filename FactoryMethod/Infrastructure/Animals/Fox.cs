777ec4a382ef2f515f8c14abc1536b5d5225ee70
Daniel Shelestov <fellowers.flobot@gmail.com>
2024-04-09 23:56:32 +0300

    add animal concrete classes

---
+++ FactoryMethod/Infrastructure/Animals/Fox.cs
@@ -0,0 +1,12 @@
+﻿using FactoryMethod.Infrastructure.Interfaces;
+
+namespace FactoryMethod.Infrastructure.Animals
+{
+    public class Fox : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("Ring-ding-ding-ding-dingeringeding");
+        }
+    }
+}
