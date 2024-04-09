cec651a0c324b8b00e19dacdc5bf2f4164f05686
Daniel Shelestov <fellowers.flobot@gmail.com>
2024-04-09 22:52:52 +0300

    add concrete implementations of beverages

---
+++ Decorator/Infrastructure/Drinks/Espresso.cs
@@ -0,0 +1,10 @@
+﻿namespace Decorator.Infrastructure.Drinks
+{
+    public class Espresso : Beverage
+    {
+        public override int Cost()
+        {
+            return 5;
+        }
+    }
+}
