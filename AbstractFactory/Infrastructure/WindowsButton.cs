ed3701cc331214ee5f0a3cf827bfc03686046278
Daniel Shelestov <fellowers.flobot@gmail.com>
2024-04-10 00:29:54 +0300

    add simple concrete classes

---
+++ AbstractFactory/Infrastructure/WindowsButton.cs
@@ -0,0 +1,9 @@
+﻿using AbstractFactory.Infrastructure.Interfaces;
+
+namespace AbstractFactory.Infrastructure
+{
+    public class WindowsButton : IProductB
+    {
+        public string Name => "Windows Button";
+    }
+}
