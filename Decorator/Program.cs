ad149850bbbac2c29c6913d9e3d61999edab136d
Daniel Shelestov <fellowers.flobot@gmail.com>
2024-04-09 23:00:31 +0300

    add simple usage example

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Decorator.Infrastructure;
+using Decorator.Infrastructure.Addons;
+using Decorator.Infrastructure.Drinks;
+
+var espresso = MakeEspresso();
+
+Console.WriteLine($"Base cost of {nameof(Espresso)} is {espresso.Cost()}$.");
+
+var espressoWithCaramel = AddCaramel(espresso);
+var espressoWithDoubleCaramel = AddCaramel(espressoWithCaramel);
+
+Console.WriteLine($"Cost of {nameof(Espresso)} with x2 caramel addons is {espressoWithDoubleCaramel.Cost()}$.");
+
+
+
+Beverage MakeEspresso()
+{
+    return new Espresso();
+}
+
+Beverage AddCaramel(Beverage beverage)
+{
+    return new Caramel(beverage);
+}
