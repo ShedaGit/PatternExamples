b531ddb3a15fbf0c86014b59c4fd55e80ddedbce
Daniel Shelestov <fellowers.flobot@gmail.com>
2024-04-10 20:23:12 +0300

    add usage examples

--- Command/Program.cs
+++ Command/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Command.Infrastructure;
+
+var light = new Light();
+
+var lightOnCommand = new LightOnCommand(light);
+var lightOffCommand = new LightOffCommand(light);
+
+var casualInvoker = new Invoker(lightOnCommand, lightOffCommand);
+var invertedInvoker = new Invoker(lightOffCommand, lightOnCommand);
+
+casualInvoker.ClickOn();
+invertedInvoker.ClickOn();
