d66eb128387d44c8f23687de459dd1a5acad00fb
Daniel Shelestov <fellowers.flobot@gmail.com>
2024-04-10 19:39:24 +0300

    add base singleton class

---
+++ Singleton/Infrastructure/Singleton.cs
@@ -0,0 +1,11 @@
+﻿namespace Singleton.Infrastructure
+{
+    public class Singleton
+    {
+        static Singleton _instance = null!;
+
+        Singleton() { }
+
+        public static Singleton GetInstance() => _instance ??= new Singleton();
+    }
+}
