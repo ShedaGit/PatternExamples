e7900c75f829e1a60bb29bfed2b81f396c0fd0df
Daniel Shelestov <fellowers.flobot@gmail.com>
2024-04-10 20:06:38 +0300

    add usage example

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Singleton.Infrastructure;
+
+var chatroom = Chatroom.GetInstance();
+
+var mike = new ChatMember("Mike");
+var john = new ChatMember("John");
+
+chatroom.SendMessage(mike, "Hello, world!");
+
+chatroom.AddMember(mike);
+chatroom.AddMember(john);
+
+chatroom.SendMessage(mike, "Is anybody here?");
+chatroom.SendMessage(john, "Oh, hello there!");
+
+var members = chatroom.GetMembers().Select(m => m.Username).Aggregate((un1, un2) => un1 + ',' + un2);
+
+Console.WriteLine($"Chatroom members: {members}.");
+
+chatroom.ShowChatLog();
