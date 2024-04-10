559bac6de0c40092d782fe0989ad32281f767736
Daniel Shelestov <fellowers.flobot@gmail.com>
2024-04-10 20:22:53 +0300

    add basic light commands

---
+++ Command/Infrastructure/Light.cs
@@ -0,0 +1,9 @@
+﻿namespace Command.Infrastructure
+{
+    public class Light
+    {
+        public void On() => Console.WriteLine("Light is on.");
+
+        public void Off() => Console.WriteLine("Light is off.");
+    }
+}
